8569810291d976fc90302e3ea66aedaa9d8927e0
IssaDia <44264590+IssaDia@users.noreply.github.com>
2023-11-29 16:07:04 +0100

    changed image regnotization model

--- app/adapters/controllers/image_controller/main.py
+++ app/adapters/controllers/image_controller/main.py
@@ -7,21 +7,22 @@ from providers.ai.image_use_case_provider import get_image_use_case
 from use_cases.description_use_case import DescriptionUseCase
 from werkzeug.utils import secure_filename
 
+from dotenv import load_dotenv
+load_dotenv()
+
 import json
-import replicate
 import requests
 
 app = Flask(__name__)
-app.secret_key = "turbo_complete"
+app.secret_key =  os.getenv("FLASK_SECRET_KEY")
 
 description_use_case = DescriptionUseCase(ReplicateAPI())
 image_use_case = get_image_use_case(use_tensorflow=True, use_transformers=False)
 
-os.environ["REPLICATE_API_TOKEN"] = "r8_fG5laWlXKezHHs4KCekQm2tasBesc7S1S4MrX"
+os.environ["REPLICATE_API_TOKEN"] = os.getenv("REPLICATE_API_TOKEN")
 
 
-DATA_API_ENDPOINT = "http://127.0.0.1:5001/"
-OLLAMA_API_ENDPOINT = "http://localhost:11434/api/generate"
+DATA_API_ENDPOINT = os.getenv("DATA_API_ENDPOINT")
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -62,21 +63,7 @@ def process_image_route():
         response = requests.post(DATA_API_ENDPOINT, data=api_data)
         descriptions = response.json()
         descriptions_text = json.dumps(descriptions)
-        prompt_input = f"You are seller on Amazon e-commerce.Generate an attractive product description based on this product : {keyword[0]} with a tone similar to the following product descriptions:\n\n{descriptions_text}"
-
-        ollama_data = {
-             'model': "mistral",
-            'prompt': prompt_input
-        }
-
-        response = requests.post(OLLAMA_API_ENDPOINT, json=ollama_data,  headers={'Content-Type': 'application/json'})
-        if response.headers['Content-Type'] == 'application/json':
-            json_objects = [json.loads(obj) for obj in response.text.split('\n') if obj.strip() != ""]
-            responses = [obj.get('response', '') for obj in json_objects]
-            description_string = ' '.join(responses)
-            print(description_string)
-        else:
-            pass
+        description_string = description_use_case.execute(descriptions_text, keyword)
         return jsonify(description=description_string)
     else:
         flash("File not allowed")
